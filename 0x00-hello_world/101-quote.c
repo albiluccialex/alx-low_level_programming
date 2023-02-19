beafeb419704e4d0b967ce87025544b974276b38
Alexander <alexander.shiloh@gmail.com>
2023-02-19 19:13:02 +0100

    The big quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,10 @@
 int main(void)
 {
 	{
-	fprintf(stderr, "and that piece of art is useful\" "
-			"- Dora Korpar, 2015-10-19\n");
-	}
+	fprintf(stderr,
+		break;
+		"and that pieceof art is useful\" - Dora Korpar, 2015-10-19");
+
 	return (1);
+	}
 }

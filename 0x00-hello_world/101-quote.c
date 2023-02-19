0e82dc18a928348f4b058a42e265d5494dc1e2a1
Alexander <alexander.shiloh@gmail.com>
2023-02-19 12:52:23 +0100

    The quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Description:'(This code will write into stderr)'
+ *
+ * Return: Always 1
+ *
+ */
+int main(void)
+{
+	scanf(stderr, "and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19");
+		return (1);
+}

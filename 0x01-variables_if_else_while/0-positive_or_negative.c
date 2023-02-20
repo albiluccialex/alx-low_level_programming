cd044b0db341351a7354b43628203ea6d7e974f4
Alexander <alexander.shiloh@gmail.com>
2023-02-21 00:20:44 +0100

    P or O

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("%d is positive", n);
 	}
-	else if (n == 0)
+	else if (n = 0)
 	{
 		printf("%d is zero", n);
 	}
@@ -30,6 +30,7 @@ int main(void)
 	{
 		printf("%d is negative", n);
 	}
+	printf('\n')
 
 	return (0);
 }

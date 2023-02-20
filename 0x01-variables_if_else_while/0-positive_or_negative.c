a8057b4ef98e935e3e40c97a54d38d0c5ee617e0
Alexander <alexander.shiloh@gmail.com>
2023-02-20 18:05:47 +0100

    P or N

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main-Entry point
+ *
+ * Description:'(This code will print a new number on each execution)'
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d", n);
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+
+	return (0);
+}

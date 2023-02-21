ae2106da04197a05bb616c5a6cbae5a6add7f6f8
Alexander <alexander.shiloh@gmail.com>
2023-02-21 01:57:04 +0100

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Description: This program will print alphabet
+ *
+ * Return: Will return 0
+ */
+int main(void)
+{
+	int c;
+	for c = 'a'; c <= '2'; c++;
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+)

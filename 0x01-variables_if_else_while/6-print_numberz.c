09bc39d3afb28e226dfb4bddefb5f31542b6c762
Alexander <alexander.shiloh@gmail.com>
2023-02-21 03:43:44 +0100

    Tebahpla

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main-Entry point
+ *
+ * Description: The code will print more numbers
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int b;
+
+	for (b = '0'; b <= '9'; b++)
+
+	{
+		putchar("%d", b);
+	}
+		putchar('\n');
+
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main-Entry
+ *
+ * Description: This code will print tebahpla
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int d;
+
+	for (d = 'z'; d >= 'a'; d--)
+
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+}

84f6ac60542406882e8d8735416b2673416144ac
Alexander <alexander.shiloh@gmail.com>
2023-02-21 02:42:47 +0100

    Add

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,17 @@
  * Return: Always 0
  */
 int main(void)
+
 {
 	int i;
-	for (i = 'A'; i <= 'Z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
 	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
 	putchar('\n');
 		return (0);
 }

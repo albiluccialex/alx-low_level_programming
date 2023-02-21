56a1ee08b53b5a0d01d78e78bbdd7398eb727081
Alexander <alexander.shiloh@gmail.com>
2023-02-21 03:10:20 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main-Entry point
+ *
+ * Description: This code will print numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+
+	int a;
+
+	for (a = 0; a < 10; a++)
+
+	{
+		printf("%d", a);
+	}
+		return (0);
+}

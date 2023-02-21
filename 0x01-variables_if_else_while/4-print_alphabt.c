682824ae78fff5d8f8dcbaef3e0de27d673f0526
Alexander <alexander.shiloh@gmail.com>
2023-02-21 02:57:23 +0100

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib>
+/**
+ * main-Entry
+ *
+ * Description: This code will print more alphabets
+ *
+ * Return: Will be 0
+ */
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+		putchar('\n');
+		return (0);
+}

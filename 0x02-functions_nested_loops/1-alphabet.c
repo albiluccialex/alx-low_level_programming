b7d1583a2777dddc7c4d5b89bbb066451e36ea84
Alexander <alexander.shiloh@gmail.com>
2023-02-25 10:54:52 +0100

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,13 +11,13 @@
 int main(void)
 
 {
-	char i = "a";
-	char j = "z";
+	char i = 'a';
+	char j = 'z';
 
 	{
 
 	while (i <= j)
-		_putchar("%c, i", i++);
+		_putchar('i', i++);
 	}
 	_putchar('\n');
 	return (0);

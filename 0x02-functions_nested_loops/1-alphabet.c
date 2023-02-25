a9188a9bf24e09ec73724fa0e5e9f8a4f2688d12
Alexander <alexander.shiloh@gmail.com>
2023-02-25 10:14:09 +0100

    The alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main- Entry
+ *
+ * Description: This code will print the alphabet.
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char a;
+	char z;
+
+	while (char a <= z)
+		_pucthar('%c', a, a++);
+
+	return (0);
+}

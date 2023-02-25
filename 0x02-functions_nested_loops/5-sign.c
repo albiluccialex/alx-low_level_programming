8c89207f6eeba14d27d426746a88cf69f45971b6
Alexander <alexander.shiloh@gmail.com>
2023-02-25 21:43:34 +0100

    Signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,16 +15,14 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		printf("+%d", n);
-		return (1);
 	}
 	else if (n == 0)
 	{
 		printf("0");
-		return (0);
 	}
 	else
 	{
-		printf("-%d", n);
+		printf("-%d", -n);
 	}
 	return (0);
 }

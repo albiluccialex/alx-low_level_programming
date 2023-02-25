8fdf0ef97f6ad459daabf4da029a60124110153b
Alexander <alexander.shiloh@gmail.com>
2023-02-25 12:46:50 +0100

    Ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- Entry
+ *
+ * Description: Will print x10 alphabet
+ *
+ * Return: 0
+ */
+
+print_alphabet_x10(void)
+
+{
+	int k;
+	char i = 'a';
+	char j;
+
+	for (k = 1; k <= 10; k++)
+	{
+		for (j = 'z'; i <= 'z'; i++)
+			_putchar(i);
+	}
+	_putchar("\n");
+}

06ac4cf1c4cb0c51424afeefa497f62d599dc3f0
Alexander <alexander.shiloh@gmail.com>
2023-03-01 03:03:29 +0100

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * void _puts- Prints a string followed by new line
+ *
+ * @str: parameter member
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}

5dbd349634a785ffe7bd94c1a800d96a8b970edb
Alexander <alexander.shiloh@gmail.com>
2023-03-01 03:32:05 +0100

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string- Revrses a string
+ *
+ * @s: Member of parameter
+ *
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int g = 0;
+
+	while (s[g] != '\0')
+	{
+		g++;
+	}
+}

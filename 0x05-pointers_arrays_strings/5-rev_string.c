eaa0d79e379c32d67c2c0f3797f1f6426871eea8
Alexander <alexander.shiloh@gmail.com>
2023-03-01 03:42:34 +0100

    Rev String

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,23 @@
  */
 void rev_string(char *s)
 {
-	int g = 0;
+	int g, stlen, hldr, prtstr;
+
+	g = 0;
 
 	while (s[g] != '\0')
 	{
 		g++;
 	}
+	stlen = g - 1;
+	prtstr = stlen / 2;
+	
+	for(g = 0; g <= prtstr; g++)
+	{
+		hldr = s[g];
+		s[g] = s[stlen];
+		s[stlen] = hldr;
+		stlen--;
+	}
+
 }

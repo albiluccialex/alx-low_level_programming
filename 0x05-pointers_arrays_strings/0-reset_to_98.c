ba5a6cf1f339afe2c204b3ce8ed745bae8863e56
Alexander <alexander.shiloh@gmail.com>
2023-03-01 02:40:53 +0100

    The reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98(int *n)- Entry point
+ * reset_to_98- changes value of a variable
  *
- * description: Changes value of variable
+ * @n: parameter member
  *
  * Return: 0
  */

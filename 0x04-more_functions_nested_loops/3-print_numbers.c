04f9badcf09714038ccf8f61da1c4763bc452c1f
Alexander <alexander.shiloh@gmail.com>
2023-03-04 09:00:39 +0100

    This code will print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Will print numbers from 0-9
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+
+{
+	int num = 0
+		while (num <= 9)
+			_putchar(num++);
+	_putchar('\n');
+}

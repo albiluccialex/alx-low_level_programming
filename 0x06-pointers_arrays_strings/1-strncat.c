0503fd92adfed75170c9b1977a53a52b8b31cfad
Alexander <alexander.shiloh@gmail.com>
2023-03-06 09:30:21 +0100

    The program will concatenate two strings but will not null-terminate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - Concatenates two strings but will not
+ * need to null-terminate
+ * @dest: destination string
+ * @src: Source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int j, k;
+
+	j = 0;
+	k = 0;
+
+	while (dest[j] != '\0')
+		j++;
+
+	while (src[k] != '\0' && k < n)
+	{
+		dest[j] = src[k];
+		j++;
+		k++;
+	}
+	dest[j] = '\0';
+
+	return (dest);
+}

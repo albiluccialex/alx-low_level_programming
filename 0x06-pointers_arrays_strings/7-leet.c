ed64df891d9b35c05b71f2292819b2784414dc45
Alexander <alexander.shiloh@gmail.com>
2023-03-06 16:58:34 +0100

    The program will encode a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+
+char *leet(char *s)
+
+{
+	int a, b;
+
+	char *j = "aAeEoOtT1L";
+	char *k = "4433007711";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (s[a] == j[b])
+			{
+				s[a] = k[b];
+			}
+		}
+	}
+	return (s);
+}
